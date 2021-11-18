b089278b1ca96732c6f81770ba88ac19f273affa
tynamight <toledano@chicagobooth.edu>
2021-11-18 07:18:11 +0000

    Progress on root_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
 
+  get "/", {:controller => "application", :action => "blank_square_form"}
+
+  get "/square/new", {:controller => "application", :action => "blank_square_form"}
+
+  get "/square/results", {:controller => "application", :action => "calculate_square"}
+
+  get "/square_root/new", {:controller => "application", :action => "blank_root_form"}
+
+  get "/square_root/results", {:controller => "application", :action => "calculate_square_root"}
+
 end
